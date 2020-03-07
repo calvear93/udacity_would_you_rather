d376ac016b9dda00808045dfd8d1268ce4fe9fce
Cristopher Alvear Candia <c_alvear@live.cl>
2020-03-07 20:35:14 -0300

    feat: changed navbar layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,13 +11,31 @@ class Navbar extends React.Component
         const { ...rest } = this.props;
 
         return (
-            <Grid padded centered>
-                <ButtonGroup className='navbar-menu' attached='top'>
-                    <NavbarTab icon='home' help='Home' to='/main' active />
-                    <NavbarTab icon='question' help='New Question' to='/main/newquestion' />
-                    <NavbarTab icon='list alternate' help='Leaderboard' to='/main/leaderboard' />
-                </ButtonGroup>
-            </Grid>
+            <>
+                <Grid.Column width={4} />
+                <Grid.Column width={6}>
+                    <Grid padded centered>
+                        <ButtonGroup className='navbar-menu' attached='top'>
+                            <NavbarTab icon='home' help='Home' to='/main' active />
+                            <NavbarTab icon='question' help='New Question' to='/main/newquestion' />
+                            <NavbarTab icon='list alternate' help='Leaderboard' to='/main/leaderboard' />
+                        </ButtonGroup>
+                    </Grid>
+                </Grid.Column>
+                <Grid.Column width={4}>
+                    <label>Hello, Cristopher Alvear</label>
+                    <NavbarTab
+                        className='navbar-session'
+                        icon='sign in'
+                        help='Login'
+                        to='/main/login'
+                        style={{
+                            width: '6vw',
+                            minWidth: '2rem'
+                        }}
+                    />
+                </Grid.Column>
+            </>
         );
     }
 }
--- src/views/layouts/AppLayout.jsx
+++ src/views/layouts/AppLayout.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Divider, Grid } from 'semantic-ui-react';
 import Navbar from '../../components/Navbar';
-import NavbarTab from '../../components/NavbarTab';
 import '../../styles/components/navbar.scss';
 
 class AppLayout extends React.PureComponent
@@ -11,25 +10,11 @@ class AppLayout extends React.PureComponent
         return (
             <Grid stackable centered padded className='app-container' columns='equal'>
                 <Grid.Row className='navbar-container' columns={3}>
-                    <Grid.Column width={4} />
-                    <Grid.Column width={6}>
-                        <Navbar />
-                    </Grid.Column>
-                    <Grid.Column width={4}>
-                        <label>Hello, Cristopher Alvear</label>
-                        <NavbarTab
-                            className='navbar-session'
-                            icon='sign in'
-                            help='Login'
-                            to='/main/login'
-                            style={{
-                                width: '6vw',
-                                minWidth: '2rem'
-                            }}
-                        />
-                    </Grid.Column>
+                    <Navbar />
                 </Grid.Row>
+
                 <Divider className='navbar-divider' fitted />
+
                 <Grid.Row>
                     <Grid.Column width={8}>
                         {this.props.children}
