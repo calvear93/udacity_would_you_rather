e7d0f09f4f335ceec87162c26f68de3c5fde6362
Cristopher Alvear Candia <c_alvear@live.cl>
2020-03-13 21:24:42 -0300

    feat: new question page UI

--- src/views/LoginPage.jsx
+++ src/views/LoginPage.jsx
@@ -54,43 +54,41 @@ class LoginPage extends React.Component
         const { users } = this.props;
 
         return (
-            <>
-                <Card centered fluid>
-                    <Card.Content className='login-header'>
-                        <Header className='login-title centered' as='h2'>Welcome to the Would You Rather App!</Header>
-                        <Header className='login-subtitle centered' as='h4'>Please sign in to continue</Header>
-                    </Card.Content>
+            <Card centered fluid>
+                <Card.Content className='login-header'>
+                    <Header className='login-title centered' as='h2'>Welcome to the Would You Rather App!</Header>
+                    <Header className='login-subtitle centered' as='h4'>Please sign in to continue</Header>
+                </Card.Content>
 
-                    <Card.Content>
-                        <Grid centered padded>
-                            <Image src={ Logo } size='small' />
-                        </Grid>
-                        <Header className='login-sign-in-info centered' as='h1'>Sign in</Header>
-                    </Card.Content>
+                <Card.Content>
+                    <Grid centered padded>
+                        <Image src={ Logo } size='small' />
+                    </Grid>
+                    <Header className='login-sign-in-info centered' as='h1'>Sign in</Header>
+                </Card.Content>
 
-                    <Card.Content extra>
-                        <Dropdown
-                            className='login-user-selector'
-                            placeholder='Select User'
-                            closeOnEscape
-                            fluid
-                            selection
-                            defaultValue={ this.userSelected }
-                            onChange={ this.handleChange }
-                            options={ this.formatUsers(users) }
-                        />
-                        <Button
-                            className='login-sign-in'
-                            fluid
-                            color='teal'
-                            disabled={ this.state.userSelected ? false : true }
-                            onClick={ this.onSignIn }
-                        >
+                <Card.Content extra>
+                    <Dropdown
+                        className='login-user-selector'
+                        placeholder='Select User'
+                        closeOnEscape
+                        fluid
+                        selection
+                        defaultValue={ this.userSelected }
+                        onChange={ this.handleChange }
+                        options={ this.formatUsers(users) }
+                    />
+                    <Button
+                        fluid
+                        className='login-sign-in'
+                        color='teal'
+                        disabled={ this.state.userSelected ? false : true }
+                        onClick={ this.onSignIn }
+                    >
                             Sign in
-                        </Button>
-                    </Card.Content>
-                </Card>
-            </>
+                    </Button>
+                </Card.Content>
+            </Card>
         );
     }
 }
--- src/views/NewQuestionPage.jsx
+++ src/views/NewQuestionPage.jsx
@@ -1,13 +1,95 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { Button, Card, Input, Grid, Header, Divider, Label } from 'semantic-ui-react';
+import { Logo } from '../assets/images';
+import { SessionAction, UsersAction } from '../store/actions';
+import '../styles/views/new-question.scss';
 
-class NewQuestionPage extends React.Component
+const minInputLength = 5;
+
+class NewQuestionPage extends React.PureComponent
 {
+    state = {
+        'question-one': '',
+        'question-two': ''
+    }
+
+    handleInputChange = (event) =>
+    {
+        const id = event.target.getAttribute('id');
+
+        this.setState({ [id]: event.target.value });
+    };
+
     render()
     {
         return (
-            <div className=''>
-                NEW QUESTION
-            </div>
+            <Card centered fluid>
+                <Card.Content className='question-header'>
+                    <Header className='centered' as='h2'>Create New Question</Header>
+                </Card.Content>
+
+                <Card.Content>
+                    <Grid padded>
+                        <Grid.Row>
+                            <Header as='h5'>Complete the question:</Header>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <Header as='h3'>Would you rather...</Header>
+                        </Grid.Row>
+
+                        <Grid.Row centered>
+                            <Input
+                                id='question-one'
+                                className='question-input'
+                                label={ { icon: 'question circle' } }
+                                labelPosition='right corner'
+                                placeholder='Enter Option One Text Here'
+                                onChange={ this.handleInputChange }
+                                required
+                            />
+                            {this.state['question-one'].length < minInputLength && (
+                                <Label pointing>Please enter a value of min {minInputLength} chars</Label>
+                            )}
+                        </Grid.Row>
+
+                        <Divider horizontal>
+                            <Header as='h4'>OR</Header>
+                        </Divider>
+
+                        <Grid.Row centered>
+                            <Input
+                                id='question-two'
+                                className='question-input'
+                                label={ { icon: 'question circle' } }
+                                labelPosition='right corner'
+                                placeholder='Enter Option Two Text Here'
+                                onChange={ this.handleInputChange }
+                                required
+                            />
+                            {this.state['question-two'].length < minInputLength && (
+                                <Label pointing>Please enter a value of min {minInputLength} chars</Label>
+                            )}
+                        </Grid.Row>
+                    </Grid>
+                </Card.Content>
+
+                <Card.Content extra>
+                    <Button
+                        fluid
+                        className='question-submit'
+                        color='teal'
+                        disabled={
+                            this.state['question-one'].length >= minInputLength &&
+                            this.state['question-two'].length >= minInputLength
+                                ? false : true
+                        }
+                    >
+                            Submit
+                    </Button>
+                </Card.Content>
+            </Card>
         );
     }
 }
